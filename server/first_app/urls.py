94edebebfda7aea04fed83eb7f2da6b83d23716f
Sadegh Bagherzadeh <sadeghb97@gmail.com>
2020-08-17 06:11:58 +0430

    send get and post params in requests

--- server/first_app/urls.py
+++ server/first_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from first_app import views
 
 urlpatterns = [
-    path('helloworld', views.hello_world)
+    path('helloworld', views.hello_world),
+    path('hello', views.hello)
 ]
--- server/first_app/views.py
+++ server/first_app/views.py
@@ -1,6 +1,16 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 @api_view()
 def hello_world(request):
     return Response({'message': "Hello RestDjango!!!"})
+
+@api_view(['GET', 'POST'])
+def hello(request):
+    if "name" in request.data:
+        return Response({'message': "Hello dear {}".format(request.data['name'])}, status=status.HTTP_200_OK)
+    elif "name" in request.GET:
+        return Response({'message': "Hi dear {}".format(request.GET['name'])}, status=status.HTTP_200_OK)
+    else:
+        return Response({'message': "Hello friend"}, status=status.HTTP_200_OK)
